48a2916b8e94462207a05ee3bf057213a3414757
kavyasharma2005 <kavyakavyas2005@gmail.com>
2025-11-11 21:45:51 +0530

    Initial Commit

---
+++ javatrial.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Sum
+{
+    void main()
+    {
+    Scanner sc=new Scanner(System.in);
+    int a,b,s=0;
+    System.out.print("Enter Two NUmbers");
+    a=sc.nextInt();
+    b=sc.nextInt();
+    s=a+b;
+    System.out.println("SUM:"+s);
+    }
+}
